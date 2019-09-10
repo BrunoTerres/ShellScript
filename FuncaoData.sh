9e4e4daaaefe5a3691b3f8e27861921a228e1e7d
BrunoTerres <brunoeterres@gmail.com>
2019-09-10 15:30:56 -0300

    Atualizada a funcao 'formato'

--- FuncaoData.sh
+++ FuncaoData.sh
@@ -19,41 +19,45 @@ tirabarra () {
 		echo $DATASEMBARRA
 	fi 	
 }
-tirabarra $DATA
+
 
 formato () {
 	if [ "$(echo "$DATA" | grep -E '/')" ] 
 	then 
-		if [ "${DATA:0:2}" -gt 12 ]  
+		tirabarra $DATA
+		if [ "${DATASEMBARRA:0:2}" -gt 12 -a "${DATASEMBARRA:2:2}" -lt 31 ]
 		then
-			echo com barra 
-			echo 0
+			echo "com barra"
+			echo "formato BR return 0"
 			return 0
-		elif [ "${DATA:3:4}" -gt 12 ]
+		elif [ "${DATASEMBARRA:0:2}" -lt 31 -a "${DATASEMBARRA:2:2}" -gt 12 ]
 		then 	
-			echo com barra
-			echo 1
+			echo "com barra"
+			echo "formato USA return 1"
 			return 1
 		else 
-			echo calma 	
+			echo "nada"
 		fi
-	else
-		if [ "${DATA:0:2}" -gt 12 -a "${DATA:2:2}" -lt 31 ]
-		then
-			echo sem barra
-			echo 0
+	else 
+		if [ "${DATA:0:2}" -gt 12 -a "${DATA:2:2}" -lt 31 ] 
+		then 
+			echo "sem barra"
+			echo "formato Brasil return 0" 
 			return 0
-		elif [ "${DATA:0:2}" -lt 31 -a "${DATA:2:2}" -gt 12 ]
-		then 	
-			echo sem barra
-			echo 1
-			return 1
+		elif [ "${DATA:0:2}" -lt 31 -a "${DATA:2:2}" -gt 12 ] 
+		then 
+			echo "sem barra" 
+			echo "formato USA return 1"
+			return 1 
 		else 
-			echo nada
-		fi
+			echo "nada2"
+		fi 
+	fi 	
  
-	fi
 } 
+formato $DATA
+
+
 checabarra () {
 	if [ $(echo "$DATA" | egrep '/') ]
 	then 	
