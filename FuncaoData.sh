03e78a5418564b951b78d10e5b034a74141ab260
BrunoTerres <brunoeterres@gmail.com>
2019-09-13 11:56:18 -0300

    Simplificada a função 'formato'

--- FuncaoData.sh
+++ FuncaoData.sh
@@ -21,51 +21,35 @@ tirabarra () {
 }
 
 formato () {
-	if [ "$(echo "$DATA" | grep -E '/')" ] 
+	tirabarra $DATA
+	if [ "${DATA:0:2}" -gt 12 -a "${DATA:2:2}" -gt 12 ]
+	then
+		echo "Formato invalido"
+		echo "return 3"
+		return 3
+	elif [ "${DATA:0:2}" -le 12 -a "${DATA:2:2}" -le 12 ]
 	then 
-		tirabarra $DATA
-		if [ "${DATA:0:2}" -gt 12 -a "${DATA:2:2}" -gt 12 ]
-		then
-			echo "Formato invalido"
-			echo "return 3"
-			return 3
-		elif [ "${DATA:0:2}" -le 12 -a "${DATA:2:2}" -le 12 ]
-		then 
-			echo "Não é possível determinar!" 
-			return 2
-		elif [ "${DATA:0:2}" -lt 31 -a "${DATA:2:2}" -lt 12 ]
-		then 	
-			echo "com barra"
-			echo "formato BR return 0"
-			return 0
-		elif [ "${DATA:0:2}" -lt 12 -a "${DATA:2:2}" -lt 31 ]
-		then 
-			echo "com barra"
-		        echo "Formato USA return 1"
-			return 1	
-		else 
-			echo "Não é possível determinar!"
-			return 2
-		fi
+		echo "Não é possível determinar!" 
+		return 2
+	elif [ "${DATA:0:2}" -lt 31 -a "${DATA:2:2}" -lt 12 ]
+	then 	
+		echo "com barra"
+		echo "formato BR return 0"
+		return 0
+	elif [ "${DATA:0:2}" -lt 12 -a "${DATA:2:2}" -lt 31 ]
+	then 
+		echo "com barra"
+	        echo "Formato USA return 1"
+		return 1	
 	else 
-		if [ "${DATA:0:2}" -gt 12 -a "${DATA:2:2}" -lt 31 ] 
-		then 
-			echo "sem barra"
-			echo "formato Brasil return 0" 
-			return 0
-		elif [ "${DATA:0:2}" -lt 31 -a "${DATA:2:2}" -gt 12 ] 
-		then 
-			echo "sem barra" 
-			echo "formato USA return 1"
-			return 1 
-		elif [ "${DATA:0:2}" -gt 31 -o "${DATA:2:2}" -gt 31 ]
-		then 
-			echo "Formato inválido!"
-		else 
-			echo "Não é possível determinar!"
-		fi 
+		echo "Não é possível determinar!"
+		return 2
 	fi 	
  
-} 
+}
+
+inverteformato () {
+	
+}
 formato $DATA
 separando $DATA
